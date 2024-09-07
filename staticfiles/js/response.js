c75d148ae723b4fdba41bf169f0bcf78ec65a8c2
Eris <johnjefferson.lebasora@evsu.edu.ph>
2024-09-07 09:22:02 +0800

    adding staticfiles folder

---
+++ staticfiles/js/response.js
@@ -0,0 +1,23 @@
+const responses = {
+	'hello': 'Hello! How can i help you today?',
+	'hi': 'Hello! How can i help you today?',
+	'thanks': "You're welcome is there anything else I can help you with?",
+	'thank you': "You're welcome is there anything else I can help you with?",
+	'thank': "You're welcome is there anything else I can help you with?",
+	'thankyou': "You're welcome is there anything else I can help you with?",
+	'are you': "I am just a chatbot created by the creator of the system. Just let me know if there's something I can assist you with.",
+	'sorry': "its ok",	
+	'none': "ok, Just let me know if theres anything I can assist you with.",
+	"no" : "ok, Just let me know if theres anything I can assist you with.",
+	"nothing" : "ok, Just let me know if theres anything I can assist you with.",
+	'ok': "Just let me know if theres anything I can assist you with.",	
+	'add household member': "To add a household members, Look throuh the sidebar in your dashboard then click 'Add Member' button.",
+	'housing management?': "The housing management system is a platform designed to help manage and allocate housing for families in need",
+	'use system?': "The system is designed for homeowners as well as secretaries and Administrators involved in managin the housing management",
+	"assistance available homeowners?": "Assistance includes temporary housing, financial aid for repairs, access to support service, and guidance on rebuilding and recovery proccess.",
+	"request report maintenance":"If you wan't to report or request a maintenance for. Look to the sidebar and click 'Request maintenance' button, then fill out the information about the damage.",
+	"housing free no fee pay much" : "This housing program is free and sponsored by the government.",
+	"change profile picture" : "To change you profile picture, Look through the upper right side then click on you photo.",
+	"much household i" : `Based on your record, You have `,
+}
+
